732b1383a38882d790d06c69a5294e96d86918f8
Rustam <Isaev.Rustam.A@gmail.com>
2023-07-24 23:45:05 +0300

    test

--- src/containers/modal/modal-load-container/modal-load-container.tsx
+++ src/containers/modal/modal-load-container/modal-load-container.tsx
@@ -1,8 +1,6 @@
 import { FC, memo, useEffect, useRef, useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
-import ModalAnimationContainer from '../modal-animation-container/modal-animation-container.tsx';
-
 import { contentAnimation } from '@/components/modal/animation-style/modal-transition.ts';
 import { ModalLoad } from '@/components/modal';
 import { Portal } from '@/containers/modal';
@@ -15,24 +13,24 @@ interface ModalLoadContainerProps {
   onClose: () => void;
 }
 
-interface UseAnimationIn {
-  opened: boolean;
-}
-const useAnimationIn = ({ opened }: UseAnimationIn) => {
-  const [animationIn, setAnimationIn] = useState(false);
-
-  useEffect(() => {
-    setAnimationIn(true);
-  }, [opened]);
-
-  return { animationIn };
-};
+// interface UseAnimationIn {
+//   opened: boolean;
+// }
+// const useAnimationIn = ({ opened }: UseAnimationIn) => {
+//   const [animationIn, setAnimationIn] = useState(false);
+//
+//   useEffect(() => {
+//     setAnimationIn(true);
+//   }, [opened]);
+//
+//   return { animationIn };
+// };
 interface FProps {
   opened: boolean;
   onClose: () => void;
 }
 const F: FC<FProps> = ({ onClose, opened }) => {
-  const contentRef = useRef();
+  const contentRef = useRef<HTMLDivElement | null>(null);
   const [animationIn, setAnimationIn] = useState(false);
 
   useEffect(() => {
